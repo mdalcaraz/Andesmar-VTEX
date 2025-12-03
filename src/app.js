e1757c646dccc2df21fc96e0e058a6ade913a3af
mdalcaraz <mdalcaraz@live.com>
2025-12-03 17:55:22 -0300

    fix iis error

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import { errorHandler } from './middlewares/error.middleware.js'
 import { notFound } from './middlewares/notFound.middleware.js'
 
 const app = express()
-
+app.set('trust proxy', true);
 app.use(helmet())
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({ extended: true }))
